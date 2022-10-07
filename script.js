084896171188c49683195222dc8693bb1b670b1f
everson dias <dev@DESKTOP-229V1QG.localdomain>
2022-10-07 15:48:33 -0300

    complited script

--- script.js
+++ script.js
@@ -3,15 +3,17 @@ const list = document.getElementById('lista-tarefas');
 const button = document.getElementById('criar-tarefa');
 const clear = document.getElementById('apaga-tudo');
 const clearCompleted = document.getElementById('remover-finalizados');
-const save = document.getElementById('salver-tarefas');
+const save = document.getElementById('salvar-tarefas');
+const moveUp = document.getElementById('mover-cima');
+const moveDow = document.getElementById('mover-baixo');
+const removeSelected = document.getElementById('remover-selecionado');
 
 const selectedItem = (event) => {
-  event.target.className = 'selected';
+  event.target.classList.add('selected');
   for (const item of list.children) {
     if (item !== event.target) {
       item.classList.remove('selected');
     }
-
   }
 }
 
@@ -23,15 +25,22 @@ const completedItem = (event) => {
   }
 }
 
-const createItem = (props) => {
-  const item = document.createElement('li');
-  item.addEventListener('click', selectedItem);
-  item.addEventListener("dblclick", completedItem);
-  item.innerHTML = props;
-  list.appendChild(item);
+const createEvent = () => {
+  const listItem = document.querySelectorAll('#lista-tarefas li');
+  for (const item of listItem) {
+    item.addEventListener('click', selectedItem);
+    item.addEventListener('dblclick', completedItem);
+  }
 }
 
-button.addEventListener('click', ()=>{
+const createItem = (props) => {
+  const item = document.createElement('li');
+  item.innerHTML = props;
+  list.appendChild(item);
+  createEvent();
+}
+
+button.addEventListener('click', () => {
   const item = input.value;
   createItem(item);
   input.value = innerHTML = '';
@@ -48,10 +57,67 @@ clearCompleted.addEventListener('click', () => {
   }
 })
 
-save.addEventListener('click', () => {
-  const saveList =  []
-  for (const item of list.children) {
-    saveList.push(item.innerHTML);
-    localStorage.setItem('list', JSON.stringify(saveList));
-  }
+removeSelected.addEventListener('click', () => {
+  const selected = document.querySelector('.selected');
+    list.removeChild(selected)
 })
+
+save.addEventListener('click', () => {
+  const saveList = []
+  for (const item of list.children) {
+    saveList.push(`<li class='${item.className}'>${item.innerHTML}</li>`);
+  }
+  localStorage.setItem('list', JSON.stringify(saveList));
+})
+
+const createList = (listChanger) => {
+  for (const itemList of listChanger) {
+    list.insertAdjacentHTML('beforeend', `<li class='${itemList.className}'>${itemList.innerHTML}</li>`);
+  }
+  createEvent()
+}
+
+const changerPosition = (myList, from, to) => {
+  myList.splice(to, 0, myList.splice(from, 1)[0]);
+  return myList;
+}
+
+const move = (props) => {
+  const listChanger = [];
+  let itemSelected;
+  let position;
+  for (const item of list.children) {
+    listChanger.push(item);
+  }
+  for (const key in listChanger) {
+    if (listChanger[key].classList[0] === 'selected') {
+      itemSelected = key
+    }
+  }
+  let parsItemSelectedFromNumber = parseInt(itemSelected)
+
+  if (props === 1) {
+    position = parsItemSelectedFromNumber -= 1;
+  } else {
+    position = parsItemSelectedFromNumber += 1;
+  }
+
+  changerPosition(listChanger, itemSelected, position);
+  list.innerHTML = '';
+  createList(listChanger)
+}
+
+moveUp.addEventListener('click', () => {
+  move(1)
+})
+moveDow.addEventListener('click', () => {
+  move(0)
+})
+
+if (localStorage.key('list')) {
+  let listObj = JSON.parse(localStorage.getItem('list'));
+  for (const item of listObj) {
+    list.insertAdjacentHTML('beforeend', item);
+  }
+  createEvent()
+}
